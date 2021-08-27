7505d8c6c05ab585eff85a16f9e36a45e4d0baa5
remi carette <carette.remi@gmail.com>
2021-08-27 10:26:45 +0200

    CI test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,11 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should compute the full_name with capitalize first_name & last_name" do
+    paul = User.new(first_name: "pAul", last_name: "tesson")
+    tom = User.new(first_name: "tom", last_name: "cresson")
+    assert_equal "Paul Tesson", paul.full_name
+    assert_equal "Tom Cresson", tom.full_name
+  end
 end
